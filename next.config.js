7e58faa6f87166314d467e5e5671bc9066f8de4d
Kenil09 <keniltogadiya100@gmail.com>
2024-01-08 15:17:09 +0530

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  // Add basePath
-  basePath: "/search-books-demo",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
